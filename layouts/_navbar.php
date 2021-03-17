e79faa9add8021c07d2b5c700b894b9c0ce3d972
Pauline GB <pauline.gb@protonmail.com>
2021-03-17 21:52:25 +0100

    ajout du header avec logo, titre, sous-titre et début de navigation.

--- layouts/_navbar.php
+++ layouts/_navbar.php
@@ -1,12 +1,30 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="../assets/style.css">
-</head>
-<body>
-    
-
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Linus Torvald</title>
+        <link rel="stylesheet" href="../assets/style.css">
+    </head>
+    <body>
+        <header>
+            <div class="bloc_title_header">
+                <div id="logo">
+                    <img src="../assets/images/pinguLogo.png" alt="logo_pingu"/>
+                </div>
+                <div class="title_header">
+                    <h1>Linus Torvald</h1>
+                    <h2>Portfolio</h2>
+                </div>
+            </div>
+            <nav>
+                <ul>
+                    <li><a href="../index.php">Accueil</a></li>
+                    <li><a href="../projet.php">Projets</a></li>
+                    <li><a href="../contact.php">Contact</a></li>
+                </ul>
+            </nav>
+        </header>
+    </body>
+</html>
