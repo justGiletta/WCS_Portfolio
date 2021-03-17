170b63c7024c92c147edef75cf94a6543685f6a9
C1NM1G <colin.moralegac@gmail.com>
2021-03-17 18:37:34 +0100

    ajout des logos/bordure footer

--- layouts/_footer.php
+++ layouts/_footer.php
@@ -1,6 +1,20 @@
+<html>
+<body>
+<link rel="stylesheet" href="../assets/style.css">
+
 <footer>
+
+    <section class="footer_section">
+
+        <div class="icon"><img src="../assets/images/facebook.png" alt="Facebook"/></div>
+        <div class="icon"><img src="../assets/images/instagram.png" alt="Twitter"/></div>
+        <div class="icon"><img src="../assets/images/linkedin.png" alt="Vimeo"/></div> 
+        <div class="icon"><img src="../assets/images/twitter.png" alt="Flickr"/></div>
+        
+    </section>
+
 </footer>
 
-<link rel="stylesheet" href="../assets/main.js">
 </body>
+<link rel="stylesheet" href="../assets/main.js">
 </html>
