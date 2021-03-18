5492a98a9fa6e59f67eafaa978c7422ddb0f26d2
DonTal-Code <Wissam.talebpro@gmail.com>
2021-03-18 09:41:21 +0100

    Creation de la page Projet

---
+++ projet.php
@@ -0,0 +1,66 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+        <link rel="stylesheet" type="text/css" href="style.css" />
+</head>
+<body>
+        
+<?php include 'layouts/_navbar.php'?>
+
+<section id = "section1" class="bloc_projet">
+<h2>Projet 1 : Linux</h2>
+<div class="boxprojet">
+<img    class="images" 
+        src="https://www.onlogic.com/company/io-hub/wp-content/uploads/2009/01/Read-only-Linux.jpg" 
+        alt="Linux_img"> 
+
+<p> Linux is a family of open-source Unix-like operating systems based on the Linux Kernel, an operating system kernel first released on septembre 17, 1991. Linux is typically packaged in a linux distrubution.
+
+In 1991, while attending the University of Helsinki, Torvalds became curious about operating systems.[48] Frustrated by the licensing of MINIX, which at the time limited it to educational use only,[47] he began to work on his own operating system kernel, which eventually became the Linux kernel.
+
+Torvalds began the development of the Linux kernel on MINIX and applications written for MINIX were also used on Linux. Later, Linux matured and further Linux kernel development took place on Linux systems.[49] GNU applications also replaced all MINIX components, because it was advantageous to use the freely available code from the GNU Project with the fledgling operating system; code licensed under the GNU GPL can be reused in other computer programs as long as they also are released under the same or a compatible license. Torvalds initiated a switch from his original license, which prohibited commercial redistribution, to the GNU GPL.[50] Developers worked to integrate GNU components with the Linux kernel, making a fully functional and free operating system.[51] </p>
+</div>
+</section>
+
+<section id = "section2" class="bloc_projet"> 
+
+<h2>Projet 2 : GIT</h2>
+
+<div class="boxprojet">
+
+<img    class="images"
+        src="https://miro.medium.com/max/700/1*Jl2VDHVzFBDdXggRprziUg.png" 
+        alt="Git_img">
+
+<p> it is a free and open source distributed version control system designed to handle everything from small to very large projects with speed and efficiency.
+
+Git is easy to learn and has a tiny footprint with lightning fast performance. It outclasses SCM tools like Subversion, CVS, Perforce, and ClearCase with features like cheap local branching, convenient staging areas, and multiple workflows.
+</p>
+
+</section>
+
+
+<section id = "section3" class="bloc_projet">
+
+<h2>Projet 3 : Subsurface</h2>
+<div class="boxprojet">
+<img class="images" src="https://subsurface-divelog.org/wp-content/uploads/2015/10/subsurface-icon1.png" alt="Subsurface_img">
+<img class="images" src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/40/Linus_in_SCUBA_gear.jpg/450px-Linus_in_SCUBA_gear.jpg" alt="Linus_Scuba_img" >
+
+<p>
+Subsurface can plan and track single- and multi-tank dives using air, Nitrox or TriMix. It allows tracking of dive locations including GPS coordinates (which can also conveniently be entered using a map interface), logging of equipment used and names of other divers, and lets users rate dives and provide additional notes.
+
+You can tag dives and filter a dive list based on criteria including tags, locations and people with whom you were diving. You can group the dive list into trips, and edit multiple dives at the same time, making it easy to support a large number of dives.</p>
+
+</section>
+
+
+<?php include 'layouts/_footer.php'?>
+
+</body>
+</html>
