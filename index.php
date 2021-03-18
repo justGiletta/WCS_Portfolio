5be2294a3580a14c37006904082c26ec05422891
Justine <justine.giletta@gmail.com>
2021-03-18 10:18:39 +0100

    creation branche homepage / structure html homepage / css associe V1

--- index.php
+++ index.php
@@ -1,4 +1,38 @@
 <?php include 'layouts/_navbar.php'?>
-// Body/content Page HERE 
+
+<div class="bodyPresCard">
+    <div class="presentationCard">
+        <div class="linusPres">
+            <h1 class="linusPres-title">Linus Torvalds</h1>
+            <div class="linusPres-text">
+                <p> Informaticien américano-finlandais, je suis plutôt connu pour avoir créé en 1991 le noyau Linux dont je continue de diriger le développement. À ce titre, je bénéficie depuis 2009 du titre de &laquo;&nbsp;Dictateur bienveillant à vie&nbsp;&raquo; de Linux.<br> J'ai également crée le logiciel de gestion de versions décentralisée Git ainsi que le logiciel d'enregistrement et de planification des plongées Subsurface.<br></J>
+
+                <p>Mais, je suis avant tout un grand amateur de pingouins&nbsp;!</p>
+            </div>
+        </div>
+        <div class="linusPres-img"><img src="https://1.bp.blogspot.com/-MonZITZk2y8/VFeRrT2bR6I/AAAAAAAAAG8/2kAAeyUWfEk/s1600/linus2_wideweb__470x300%2C0.jpg" width=800px heigth=600px alt="Photo Linus Torvalds"/></img></div>
+    </div>
+</div>
+
+<h1 class="projectsCard-title">Projets</h1>
+
+<div class="bodyProjectsCard">
+    <div class="projectCard">
+        <h2 class="projectCard-title">Linux</h2>
+        <div class="pictolinux-img"><img src="/assets/images/linuxJaune.png" width=100px heigth=100px alt="Linux logo Pictogramme"/></img></div>
+        <div class="project-img"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSWcl2jX2P3jsPv9H8lD9wFjcBZdp_yu85fFw&usqp=CAU" width=800px heigth=600px alt="Linux Logo"/></img></div>
+    </div>
+    <div class="projectCard">
+        <h2 class="projectCard-title">Git</h2>
+        <div class="pictolinux-img"><img src="/assets/images/linuxJaune.png" width=100px heigth=100px alt="Linux logo Pictogramme"/></img></div>
+        <div class="project-img"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e0/Git-logo.svg/1280px-Git-logo.svg.png" width=800px heigth=600px alt="Git Logo"/></img></div>
+    </div>
+    <div class="projectCard">
+        <h2 class="projectCard-title">Subsurface</h2>
+        <div class="pictolinux-img"><img src="/assets/images/linuxJaune.png" width=100px heigth=100px alt="Linux logo Pictogramme"/></img></div>
+        <div class="project-img"><img src="https://seeklogo.com/images/S/subsurface-logo-08EF58940C-seeklogo.com.png" width=150px heigth=50px alt="Subsurface Logo"/></img></div>
+    </div>
+
+</div>
 
 <?php include 'layouts/_footer.php'?>
