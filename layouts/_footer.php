af179692da1fac3724feb37592ee3395269d1740
C1NM1G <colin.moralegac@gmail.com>
2021-03-18 10:31:20 +0100

    ajout des links aux icons

--- layouts/_footer.php
+++ layouts/_footer.php
@@ -2,10 +2,10 @@
 
     <section class="footer_section">
 
-        <div class="icon"><img src="../assets/images/facebook.png" alt="Facebook"/></div>
-        <div class="icon"><img src="../assets/images/instagram.png" alt="Twitter"/></div>
-        <div class="icon"><img src="../assets/images/linkedin.png" alt="Vimeo"/></div> 
-        <div class="icon"><img src="../assets/images/twitter.png" alt="Flickr"/></div>
+        <div class="icon"><a href="https://www.facebook.com/Linus-Torvalds-47144560930" target="blank"><img src="../assets/images/facebook.png" alt="Facebook"/></a></div>
+        <div class="icon"><a href="https://www.instagram.com/best_computer_science_memes/?hl=fr" target="blank"><img src="../assets/images/instagram.png" alt="Twitter"/></a></div>
+        <div class="icon"><a href="https://www.linkedin.com/in/linustorvalds" target="blank"><img src="../assets/images/linkedin.png" alt="Instagram"/></a></div> 
+        <div class="icon"><a href="https://twitter.com/linus__torvalds?lang=fr" target="blank"><img src="../assets/images/twitter.png" alt="Twitter"/></a></div>
         
     </section>
 
@@ -16,3 +16,6 @@
 </body>
 
 </html>
+
+
+              
