02933a25342d1e33c44073c227d5df16e54cf020
Justine <justine.giletta@gmail.com>
2021-03-19 09:12:46 +0100

    css V2 / Attention ajout de typo dans le header pour text : possibilités de conflits

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
                 <p>Mais, je suis avant tout un grand amateur de pingouins&nbsp;!</p>
             </div>
         </div>
-        <div class="linusPres-img"><img src="https://1.bp.blogspot.com/-MonZITZk2y8/VFeRrT2bR6I/AAAAAAAAAG8/2kAAeyUWfEk/s1600/linus2_wideweb__470x300%2C0.jpg" width=800px heigth=600px alt="Photo Linus Torvalds"/></img></div>
+        <div class="linusPres-img"><img src="https://1.bp.blogspot.com/-MonZITZk2y8/VFeRrT2bR6I/AAAAAAAAAG8/2kAAeyUWfEk/s1600/linus2_wideweb__470x300%2C0.jpg" width=800px heigth=600px alt="Young Linus Torvalds"/></img></div>
     </div>
 </div>
 
@@ -19,18 +19,17 @@
 <div class="bodyProjectsCard">
     <div class="projectCard">
         <h2 class="projectCard-title">Linux</h2>
-        <div class="pictolinux-img"><img src="/assets/images/linuxJaune.png" width=100px heigth=100px alt="Linux logo Pictogramme"/></img></div>
-        <div class="project-img"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSWcl2jX2P3jsPv9H8lD9wFjcBZdp_yu85fFw&usqp=CAU" width=800px heigth=600px alt="Linux Logo"/></img></div>
+        <div class="project-img"><img src="assets/images/linuxPicto.png" width=800px heigth=600px alt="Linux Logo"/></img></div>
     </div>
     <div class="projectCard">
-        <h2 class="projectCard-title">Git</h2>
-        <div class="pictolinux-img"><img src="/assets/images/linuxJaune.png" width=100px heigth=100px alt="Linux logo Pictogramme"/></img></div>
+        <div class="projectCard-title"><h2>Git</h2>
+        <img id="picto" src="/assets/images/linuxJaune.png" width=150px heigth=150px alt="Subsurface Logo"/></img>
+        </div>
         <div class="project-img"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e0/Git-logo.svg/1280px-Git-logo.svg.png" width=800px heigth=600px alt="Git Logo"/></img></div>
     </div>
     <div class="projectCard">
         <h2 class="projectCard-title">Subsurface</h2>
-        <div class="pictolinux-img"><img src="/assets/images/linuxJaune.png" width=100px heigth=100px alt="Linux logo Pictogramme"/></img></div>
-        <div class="project-img"><img src="https://seeklogo.com/images/S/subsurface-logo-08EF58940C-seeklogo.com.png" width=150px heigth=50px alt="Subsurface Logo"/></img></div>
+        <div class="project-img"><img src="https://seeklogo.com/images/S/subsurface-logo-08EF58940C-seeklogo.com.png" width=150px heigth=150px alt="Subsurface Logo"/></img></div>
     </div>
 
 </div>
--- layouts/_navbar.php
+++ layouts/_navbar.php
@@ -6,6 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="../assets/style.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@400;700&display=swap" rel="stylesheet">
 </head>
 <body>
     
