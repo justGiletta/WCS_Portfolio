96dbfd8599df1a5bc536fe82ad5b03085cb708fb
C1NM1G <colin.moralegac@gmail.com>
2021-03-19 10:07:17 +0100

    refont totale du footer (html/css)en responsive

--- layouts/_footer.php
+++ layouts/_footer.php
@@ -1,21 +1,71 @@
-<footer>
+<!-- /* ********* DEBUT DU FOOTER (DEV : COLIN) ******** */ -->
 
-    <section class="footer_section">
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.9.0/css/all.css">
 
-        <div class="icon"><a href="https://www.facebook.com/Linus-Torvalds-47144560930" target="blank"><img src="../assets/images/facebook.png" alt="Facebook"/></a></div>
-        <div class="icon"><a href="https://www.instagram.com/best_computer_science_memes/?hl=fr" target="blank"><img src="../assets/images/instagram.png" alt="Twitter"/></a></div>
-        <div class="icon"><a href="https://www.linkedin.com/in/linustorvalds" target="blank"><img src="../assets/images/linkedin.png" alt="Instagram"/></a></div> 
-        <div class="icon"><a href="https://twitter.com/linus__torvalds?lang=fr" target="blank"><img src="../assets/images/twitter.png" alt="Twitter"/></a></div>
-        
-    </section>
 
-</footer>
+  <footer> 
+
+    <div id='footer_section'>
+    
+      <div class='icon'>
+        <a href='https://www.linkedin.com/in/linustorvalds' target='_blank'>
+          <div>
+          <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span>
+            
+                  <i class="fab fa-linkedin-in"></i>
+                </span>
+          </div>
+        </a>
+        <a href='https://github.com/torvalds' target='_blank' class='orange-shadow' >
+          <div>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span>
+                  <i class="fab fa-github"></i>
+                </span>
+
+          </div>
+        </a>
+        <a href='https://www.facebook.com/Linus-Torvalds-47144560930' class='orange-border' target='_blank'>
+          <div>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span>
+                  <i class="fab fa-facebook-f"></i>
+                </span>
+          </div>
+        </a>
+        <a href='https://www.instagram.com/best_computer_science_memes/?hl=fr' class='orange-background' target='_blank'>
+          <div>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span>
+                  <i class="fab fa-instagram"></i>
+                </span>
+          </div>
+        </a>
+      </div>               
+    </div>
+
+    <div class='copyright'>
+        <p>Team Pingu © </p>
+      </div>
+
+  </footer>
 
 <script src="../assets/main.js"></script>
-
 </body>
 
 </html>
 
-
-              
+<!-- /* ********* FIN DU FOOTER (DEV : COLIN) ******** */ -->              
