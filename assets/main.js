66da5c5df4435039da8c22f7dcad9bcc118af2c5
Pauline GB <pauline.gb@protonmail.com>
2021-03-19 10:21:25 +0100

    modify const name header => headerHidden

--- assets/main.js
+++ assets/main.js
@@ -3,20 +3,20 @@
 const burger = document.getElementById('burger');
 const links = document.getElementById('links');
 const quit = document.getElementById('quit');
-const header = document.getElementById('bloc_title_header');
+const headerHidden = document.getElementById('bloc_title_header');
 
 function burgerMenu(){
     burger.style.padding = '16px 16px 200vw 200vw';
     links.style.display = 'flex';
     quit.style.display = 'inline';
-    header.style.visibility = 'hidden';
+    headerHidden.style.visibility = 'hidden';
 };
   
 function quitFunction(){
     burger.style.padding = '16px 16px 32px 32px';
     links.style.display = 'none';
     quit.style.display = 'none';
-    header.style.visibility = 'visible';
+    headerHidden.style.visibility = 'visible';
 };
 
 /* Fin partie Header nav - burger menu - responsive */
