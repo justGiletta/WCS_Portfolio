89b33f58b4f82d5f650c2be6319be18100cf50ef
DonTal-Code <Wissam.talebpro@gmail.com>
2021-03-19 13:27:44 +0100

    V2 de la page Projet HTML et CSS, et ajout d'une photo Pingu dans le fichier assets

--- projet.php
+++ projet.php
@@ -1,29 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
-        <link rel="stylesheet" type="text/css" href="style.css" />
+<link rel="stylesheet" type="text/css" href="/assets/style.css"/>
 </head>
 <body>
         
-<?php include 'layouts/_navbar.php'?>
+<!-- Bloc 1 / Projet 1 -->
 
-<section id = "section1" class="bloc_projet">
-
-
-<div class="boxprojet">
-
-<img    class="images" 
-        id = "firstboximage"
-        src="https://www.onlogic.com/company/io-hub/wp-content/uploads/2009/01/Read-only-Linux.jpg" 
-        alt="Linux_img"> 
-</div>
+<div class="bloc_projet" id = "section1" >
 
 <div class="textprojet">
-
 <h2>Projet 1 : Linux</h2>
 <p> Linux is a family of open-source Unix-like operating systems based on the Linux Kernel, an operating system kernel first released on septembre 17, 1991. Linux is typically packaged in a linux distrubution.
 
@@ -31,33 +14,52 @@ In 1991, while attending the University of Helsinki, Torvalds became curious abo
 
 Torvalds began the development of the Linux kernel on MINIX and applications written for MINIX were also used on Linux. Later, Linux matured and further Linux kernel development took place on Linux systems.[49] GNU applications also replaced all MINIX components, because it was advantageous to use the freely available code from the GNU Project with the fledgling operating system; code licensed under the GNU GPL can be reused in other computer programs as long as they also are released under the same or a compatible license. Torvalds initiated a switch from his original license, which prohibited commercial redistribution, to the GNU GPL.[50] Developers worked to integrate GNU components with the Linux kernel, making a fully functional and free operating system.[51] </p>
 </div>
-</section>
-
-
-
-
-<section id = "section2" class="bloc_projet"> 
-
-
 
 <div class="boxprojet">
 
-<img    class="images"
-        id="secondboximage"
-        src="https://miro.medium.com/max/700/1*Jl2VDHVzFBDdXggRprziUg.png" 
-        alt="Git_img">
+<img    class="images" 
+        id = "firstboximage1"
+        src="https://www.leparisien.fr/resizer/WmqtmYWDu3FZdXy82jzKoXvgUg4=/932x582/arc-anglerfish-eu-central-1-prod-leparisien.s3.amazonaws.com/public/5CEWJB2TW53SBBOYNLOA3MSUWU.jpg" width=800px heigth=600px        alt="Linux_img"> 
+<img class ="images" id= "firstboximage2" src= "assets/linuxPicto.png" width=800px heigth=600px alt="Linux_Picto">
 </div>
+
+</div>
+
+<!-- Bloc 2  / Projet 2 -->
+
+<div class="bloc_projet" id = "section2"> 
+
 <div class="textprojet">
-        <h2>Projet 2 : GIT</h2>
+<h2>Projet 2 : GIT</h2>    
 <p> it is a free and open source distributed version control system designed to handle everything from small to very large projects with speed and efficiency.
 
 Git is easy to learn and has a tiny footprint with lightning fast performance. It outclasses SCM tools like Subversion, CVS, Perforce, and ClearCase with features like cheap local branching, convenient staging areas, and multiple workflows.
 </p>
+</div>
 
-</section>
+<div class="boxprojet">
+    
 
+<img    class="images"
+        id="secondboximage"
+        src="https://images-ext-1.discordapp.net/external/RGdVilbfJ7MgbBhb9QFkDBFiYbPmX8BaB9ObMcjBsAA/https/upload.wikimedia.org/wikipedia/commons/thumb/e/e0/Git-logo.svg/1280px-Git-logo.svg.png?width=960&height=401" 
+        alt="Git_img">
+</div>
 
-<section id = "section3" class="bloc_projet">
+</div>
+
+<!-- Bloc 3 / Projet 3-->
+
+<div class="bloc_projet" id = "section3">
+
+<div class="textprojet">
+<h2>Projet 3 : Subsurface</h2>
+<p>
+Subsurface can plan and track single- and multi-tank dives using air, Nitrox or TriMix. It allows tracking of dive locations including GPS coordinates (which can also conveniently be entered using a map interface), logging of equipment used and names of other divers, and lets users rate dives and provide additional notes.
+
+You can tag dives and filter a dive list based on criteria including tags, locations and people with whom you were diving. You can group the dive list into trips, and edit multiple dives at the same time, making it easy to support a large number of dives.</p>
+
+</div>
 
 <div class="boxprojet">
 
@@ -66,18 +68,8 @@ Git is easy to learn and has a tiny footprint with lightning fast performance. I
 
 </div>
 
-<div class="textprojet">
-<h2>Projet 3 : Subsurface</h2>
-
-<p>
-Subsurface can plan and track single- and multi-tank dives using air, Nitrox or TriMix. It allows tracking of dive locations including GPS coordinates (which can also conveniently be entered using a map interface), logging of equipment used and names of other divers, and lets users rate dives and provide additional notes.
-
-You can tag dives and filter a dive list based on criteria including tags, locations and people with whom you were diving. You can group the dive list into trips, and edit multiple dives at the same time, making it easy to support a large number of dives.</p>
-
-</section>
-
-
-<?php include 'layouts/_footer.php'?>
+</div> 
 
 </body>
+
 </html>
