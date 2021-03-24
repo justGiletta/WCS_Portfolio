980291abe9821b71d8494b7f59ee84c6bcad8734
DonTal-Code <Wissam.talebpro@gmail.com>
2021-03-24 16:03:40 +0100

    Ajout du Responsive

--- projet.php
+++ projet.php
@@ -6,80 +6,84 @@
 
 <body>
 
-    <!-- Bloc Video -->
-    <div class="bloc_projet" id='videoProjet'>
-        <h1 class="h1-projet"> My Projects </h1>
+<!-- Bloc Video -->
+<div class="bodyPresCard" id="bodyVideoCard">
+<div class="bloc_projet" id='videoProjet'>
+    <h1 class="h1-projet"> My Projects </h1>
+<div class=""bloc_video">
+    <iframe class="videos" width="420" height="315" src="https://www.youtube.com/embed/S5S9LIT-hdc" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    </iframe>
+</div>
+</div>
+</div>
+<!-- Bloc 1 / Projet 1 -->
+<div class="bodyPresCard">
+<div class="bloc_projet" id="section1">
 
-        <iframe class="videos" width="420" height="315" src="https://www.youtube.com/embed/S5S9LIT-hdc" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-        </iframe>
+    <div class="textbox">
+
+        <h2 class="h2-projet">Linux</h2>
+
+        <p class="p-projet">
+            Linux is a family of open-source Unix-like operating systems based on the Linux Kernel, an operating system kernel first released on septembre 17, 1991. Linux is typically packaged in a linux distrubution.
+
+            In 1991, while attending the University of Helsinki, Torvalds became curious about operating systems.[48] Frustrated by the licensing of MINIX, which at the time limited it to educational use only,[47] he began to work on his own operating system kernel, which eventually became the Linux kernel.
+
+        </p>
     </div>
-    <!-- Bloc 1 / Projet 1 -->
-    <div class="bloc_projet" id="section1">
+    <div class="imagebox">
+        <a href="https://www.linux.org/" target=_blank>
 
-        <div class="textbox">
+            <img class="images" id="firstboximage" src="https://www.leparisien.fr/resizer/WmqtmYWDu3FZdXy82jzKoXvgUg4=/932x582/arc-anglerfish-eu-central-1-prod-leparisien.s3.amazonaws.com/public/5CEWJB2TW53SBBOYNLOA3MSUWU.jpg" width=960px heigth=600px alt="Linux_img">
 
-            <h2 class="h2-projet">Linux</h2>
-
-            <p class="p-projet"> 
-                Linux is a family of open-source Unix-like operating systems based on the Linux Kernel, an operating system kernel first released on septembre 17, 1991. Linux is typically packaged in a linux distrubution.
-
-                In 1991, while attending the University of Helsinki, Torvalds became curious about operating systems.[48] Frustrated by the licensing of MINIX, which at the time limited it to educational use only,[47] he began to work on his own operating system kernel, which eventually became the Linux kernel.
-
-            </p>
-        </div>
-        <div class="imagebox">
-            <a href="https://www.linux.org/" target=_blank>
-
-                <img class="images" id="firstboximage" src="https://www.leparisien.fr/resizer/WmqtmYWDu3FZdXy82jzKoXvgUg4=/932x582/arc-anglerfish-eu-central-1-prod-leparisien.s3.amazonaws.com/public/5CEWJB2TW53SBBOYNLOA3MSUWU.jpg" width=960px heigth=600px alt="Linux_img">
-
-            </a>
-        </div>
-
+        </a>
     </div>
+</div>
+</div>
 
-    <!-- Bloc 2  / Projet 2 -->
+<!-- Bloc 2  / Projet 2 -->
+<div class="bodyPresCard">
+<div class="bloc_projet" id="section2">
 
-    <div class="bloc_projet" id="section2">
+    <div class="textbox">
+        <h2 class="h2-projet">GIT</h2>
+        <p class="p-projet">
+            Linux is a family of open-source Unix-like operating systems based on the Linux Kernel, an operating system kernel first released on septembre 17, 1991. Linux is typically packaged in a linux distrubution.
 
-        <div class="textbox">
-            <h2 class="h2-projet">GIT</h2>
-            <p class="p-projet"> 
-                Linux is a family of open-source Unix-like operating systems based on the Linux Kernel, an operating system kernel first released on septembre 17, 1991. Linux is typically packaged in a linux distrubution.
-
-                In 1991, while attending the University of Helsinki, Torvalds became curious about operating systems.[48] Frustrated by the licensing of MINIX, which at the time limited it to educational use only,[47] he began to work on his own operating system kernel, which eventually became the Linux kernel.
-            </p>
-        </div>
-
-        <div class="imagebox">
-            <a href="https://git-scm.com/" target=_blank>
-
-                <img class="images" id="secondboximage" src="https://images-ext-1.discordapp.net/external/RGdVilbfJ7MgbBhb9QFkDBFiYbPmX8BaB9ObMcjBsAA/https/upload.wikimedia.org/wikipedia/commons/thumb/e/e0/Git-logo.svg/1280px-Git-logo.svg.png?width=960&height=401" alt="Git_img">
-            </a>
-        </div>
-
+            In 1991, while attending the University of Helsinki, Torvalds became curious about operating systems.[48] Frustrated by the licensing of MINIX, which at the time limited it to educational use only,[47] he began to work on his own operating system kernel, which eventually became the Linux kernel.
+        </p>
     </div>
 
-    <!-- Bloc 3 / Projet 3-->
+    <div class="imagebox">
+        <a href="https://git-scm.com/" target=_blank>
 
-    <div class="bloc_projet" id="section3">
+            <img class="images" id="secondboximage" src="https://images-ext-1.discordapp.net/external/RGdVilbfJ7MgbBhb9QFkDBFiYbPmX8BaB9ObMcjBsAA/https/upload.wikimedia.org/wikipedia/commons/thumb/e/e0/Git-logo.svg/1280px-Git-logo.svg.png?width=960&height=401" alt="Git_img">
+        </a>
+    </div>
+</div>
+</div>
 
-        <div class="textbox">
-            <h2 class="h2-projet">Subsurface</h2>
-            <p class="p-projet">
-                Linux is a family of open-source Unix-like operating systems based on the Linux Kernel, an operating system kernel first released on septembre 17, 1991. Linux is typically packaged in a linux distrubution.
+<!-- Bloc 3 / Projet 3-->
+<div class="bodyPresCard">
+<div class="bloc_projet" id="section3">
 
-                In 1991, while attending the University of Helsinki, Torvalds became curious about operating systems.[48] Frustrated by the licensing of MINIX, which at the time limited it to educational use only,[47] he began to work on his own operating system kernel, which eventually became the Linux kernel.</p>
+    <div class="textbox">
+        <h2 class="h2-projet">Subsurface</h2>
+        <p class="p-projet">
+            Linux is a family of open-source Unix-like operating systems based on the Linux Kernel, an operating system kernel first released on septembre 17, 1991. Linux is typically packaged in a linux distrubution.
 
-        </div>
-
-        <div class="imagebox">
-            <a href="https://subsurface-divelog.org/" target=_blank>
-                <img class="images" id="imageScuba" src="https://subsurface-divelog.org/wp-content/uploads/2015/10/subsurface-icon1.png" Width=800px heigth=600px alt="Subsurface_img">
-                <!-- pic removed <img class="images" id="imageLinus" src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/40/Linus_in_SCUBA_gear.jpg/450px-Linus_in_SCUBA_gear.jpg" alt="Linus_Scuba_img" > -->
-            </a>
-        </div>
+            In 1991, while attending the University of Helsinki, Torvalds became curious about operating systems.[48] Frustrated by the licensing of MINIX, which at the time limited it to educational use only,[47] he began to work on his own operating system kernel, which eventually became the Linux kernel.</p>
 
     </div>
+
+    <div class="imagebox">
+        <a href="https://subsurface-divelog.org/" target=_blank>
+            <img class="images" id="imageScuba" src="https://subsurface-divelog.org/wp-content/uploads/2015/10/subsurface-icon1.png" Width=800px heigth=600px alt="Subsurface_img">
+            <!-- pic removed <img class="images" id="imageLinus" src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/40/Linus_in_SCUBA_gear.jpg/450px-Linus_in_SCUBA_gear.jpg" alt="Linus_Scuba_img" > -->
+        </a>
+    </div>
+</div>
+</div>
 
 </body>
 <?php include 'layouts/_footer.php'?>
